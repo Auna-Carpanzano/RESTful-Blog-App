aa1f130637dc687efe7161fc96351c61f869dfc4
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-11 16:20:28 -0700

    Add app.listen to set up local host:3000 server

--- app.js
+++ app.js
@@ -7,3 +7,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(3000, function() {
+  console.log("SERVER IS RUNNING");
+});
