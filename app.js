d9c8de15af4970d6e811f75bf861e122fd176df7
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-11 19:15:32 -0700

    Render the edit page with foundBlog data when a GET request is made to "/blogs/:id/edit"

--- app.js
+++ app.js
@@ -63,6 +63,17 @@ app.get("/blogs/:id", function(req, res) {
   });
 });
 
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("edit", {blog: foundBlog});
+    }
+  });
+});
+
 app.listen(3000, function() {
   console.log("SERVER IS RUNNING");
 });
