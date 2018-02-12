bfd68b940f81e993721b4fa57b0a636a6930e48b
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-11 18:10:04 -0700

    Create new Blog and redirect to "/blogs" when a POST request is made to "/blogs"

--- app.js
+++ app.js
@@ -41,6 +41,17 @@ app.get("/blogs/new", function(req, res) {
   res.render("new");
 });
 
+// CREATE ROUTE
+app.post("/blogs", function(req, res) {
+  Blog.create(req.body.blog, function(err, newBlog) {
+    if (err) {
+      res.render("new");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+});
+
 app.listen(3000, function() {
   console.log("SERVER IS RUNNING");
 });
